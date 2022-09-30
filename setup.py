25287fe298523356c002939501861e9b8ca3a1f4
ziang <ziangz@andrew.cmu.edu>
2022-09-30 17:04:31 -0400

    add dependencies while setup

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="easybird",
-    version="0.0.2",
+    version="0.0.3",
     author="Ziang Zhou",
     author_email="ziang.zhou518@gmail.com",
-    description="A toolkit for Bird Activity Detection (BAD)",
+    description="A python toolkit for Bird Activity Detection (BAD)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/realzza/easybird",
@@ -23,6 +23,16 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     include_package_data=True,
+    install_requires = [
+        'audiofile',
+        'torch',
+        'torchaudio',
+        'torchvision',
+        'soxr',
+        'tqdm',
+        'scipy',
+        'python_speech_features'
+    ],
     package_data = {
         'easybird': ['models/*']
     },
